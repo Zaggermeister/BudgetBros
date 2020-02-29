1f12dcd6e357bf62fa50127645cff1f969925733
Eddie Maune <eddiem@bbd.co.za>
2020-02-29 22:04:08 +0200

    First Commit

---
+++ src/test/java/com/gradwork2020/budgetbros/BudgetbrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradwork2020.budgetbros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
