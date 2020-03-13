1994d241180c62c4812511e6d8085182137afe9e
Zanrich <zzanrich@gmail.com>
2020-03-13 07:14:53 +0200

    small chnages

--- src/test/java/com/bbdgradwork/budgetbros/controller/HttpRequestTest.java
+++ src/test/java/com/bbdgradwork/budgetbros/controller/HttpRequestTest.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.junit.jupiter.api.Test;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
